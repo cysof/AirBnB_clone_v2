67f005da20d5e7c7e9b06b2c400eca1c77b755c6
cp4reallife@yahoo.com <cp4reallife@yahoo.com>
2024-02-22 01:53:39 +0100

    fixing up code

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,7 @@ from os import getenv
 from models.engine import db_storage
 from models.engine import file_storage
 
-
-db_storage_type = getenv('HBNB_TYPE_STORAGE')
+db_storage_type = getenv("HBNB_TYPE_STORAGE")
 
 if db_storage_type == 'db':
     storage = db_storage.DBStorage()
