079cc71ceaa31333b3393eb45d5762e45ef459b6
cysof <cp4reallife@yahoo.com>
2024-04-20 02:28:51 +0000

    fix tasl 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,14 +5,14 @@ Web application must be listening on 0.0.0.0, port 5000
 must use the option strict_slashes=False in your route definition
 """
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     """FUNCTION CALL THROUGH THE / ROUTE."""
-    return render_template('100-hbnb.html')
+    return "Hello HBNB!"
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=5000)
